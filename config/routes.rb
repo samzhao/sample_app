1e4efbce01abe5ec79b1ebbedcd9669797886dc1
Sam Zhao <zhao6518@gmail.com>
2012-09-27 18:10:36 -0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
-
-  get "static_pages/home"
-
-  get "static_pages/help"
-
-  get "static_pages/about"
-
-  get "static_pages/contact"
+  resources :users
 
   root to: 'static_pages#home'
+  
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
+  get "static_pages/contact"
 
   match '/signup', to: 'users#new'
 
